78c722f5ea4b3bda76223b75f6079fba71f351ef
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2025-01-06 21:32:35 +0330

    Change timeout text

--- lib/features/proxy/widget/proxy_tile.dart
+++ lib/features/proxy/widget/proxy_tile.dart
@@ -53,7 +53,7 @@ class ProxyTile extends HookConsumerWidget with PresLogger {
       ),
       trailing: proxy.urlTestDelay != 0
           ? Text(
-              proxy.urlTestDelay > 65000 ? "×" : proxy.urlTestDelay.toString(),
+              proxy.urlTestDelay > 65000 ? "timeout" : proxy.urlTestDelay.toString(),
               style: TextStyle(color: delayColor(context, proxy.urlTestDelay)),
             )
           : null,
